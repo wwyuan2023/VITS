fe17ad1e62c2897334a1b7dc457a6efc6986dbeb
wwy2023 <yuanwuwen@126.com>
2023-08-29 13:34:24 +0800

    Update

--- emotional-vits/toolkits/del_ckpt.sh
+++ emotional-vits/toolkits/del_ckpt.sh
@@ -12,18 +12,20 @@ fi
 echo "root=$root, maxno=$maxno"
 
 while true; do
-    find $root -name "*.pth" | perl -e '
-        $maxno = int($ARGV[0]) * 2;
-        while(<STDIN>){
-            chomp;
-            /[GD]_(\d+)\.pth/; 
-            push @a, [$1, $_];
-        } 
-        @b = sort {$b->[0] <=> $a->[0]} @a; 
-        for($i=$maxno; $i<=$#b; $i++){
-            print "delete $b[$i]->[1]\n";
-            unlink $b[$i]->[1];
-        }
-    ' $maxno; 
+    find $root -type d | while read cdir; do
+        find $cdir -name "*.pth" -maxdepth 1 | perl -e '
+            $maxno = int($ARGV[0]) * 2;
+            while(<STDIN>){
+                chomp;
+                /[GD]_(\d+)\.pth/; 
+                push @a, [$1, $_];
+            } 
+            @b = sort {$b->[0] <=> $a->[0]} @a; 
+            for($i=$maxno; $i<=$#b; $i++){
+                print "delete $b[$i]->[1]\n";
+                unlink $b[$i]->[1];
+            }
+        ' $maxno; 
+    done;
     sleep 300;
 done

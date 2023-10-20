1ba2a82345cf0a2b3c602405a02917ee36aa9da3
wwy2023 <yuanwuwen@126.com>
2023-10-20 13:33:15 +0800

    delete no used

--- emotional-vits2/version.py
+++
@@ -1,3 +0,0 @@
-# encoding: utf-8
-
-__verion__ = "2.0.1"
--- emotional-vits2/web_api/monitor.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-wdir=`pwd`;
-while true; do
-    # watch tts/http/tlog
-    bash ./start.sh
-    # sleep
-    sleep 60;
-done
